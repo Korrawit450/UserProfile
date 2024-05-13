9f9468642559181b39bc40094a34416a520bf14e
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-05-13 20:27:48 +0700

    Add files via upload

---
+++ src/userprofile/UserGUI.java
@@ -0,0 +1,14 @@
+package userprofile;
+import java.awt.*;
+import javax.swing.*;
+
+public class UserGUI {
+	private JFrame frame;
+	
+	public UserGUI() {
+		frame = new JFrame();
+		frame.setTitle("Java GUI");
+	}
+
+
+}
