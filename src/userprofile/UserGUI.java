89e3b202a7aebf94588ce0e915a823fecd01d448
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-05-15 21:18:23 +0700

    Add files via upload

--- src/userprofile/UserGUI.java
+++ src/userprofile/UserGUI.java
@@ -8,7 +8,13 @@ public class UserGUI {
 	public UserGUI() {
 		frame = new JFrame();
 		frame.setTitle("Java GUI");
+		frame.setSize(400, 300);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
-
+	public static void main(String[] args) {
+		new UserGUI();
+	}
 
 }
